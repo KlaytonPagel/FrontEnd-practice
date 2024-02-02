9748637bca0cce10aa5332b4e7b0896019ec55cc
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-02 11:10:22 -0600

    Made Bouncy Line
    Made a line that bounces back and fourth on the screen

--- canvas_testing/canvas.js
+++ canvas_testing/canvas.js
@@ -13,7 +13,38 @@ pen.fillStyle = "#8f3a3a";
 // Makes a rectangle filled with the fillStyle color
 pen.fillRect(width/2/2, height/2/2, width/2, height/2);
 
-// Creates a line
-pen.moveTo(0,0); // Line starting point
-pen.lineTo(width, height); // Line ending point
-pen.stroke(); // Makes the line visible
+
+
+// animates a line bouncing back end fourth_____________________________________________________________________________
+let start = 0;
+let speed = 3;
+let direction = 0;
+run();
+function run(){
+    // Clears the last frame and begins a new path
+    pen.clearRect(0,0, width, height);
+    pen.beginPath();
+
+    // Checks the direction it is moving then moves it
+    if (direction == 0) {
+        pen.moveTo(start += speed, 0); // Line starting point
+        pen.lineTo(start += speed, height); // Line ending point
+    } else if (direction == 1) {
+        pen.moveTo(start -= speed, 0); // Line starting point
+        pen.lineTo(start -= speed, height); // Line ending point
+    }
+
+    // Checks to see if it has hit the wall yet
+    if (start > width) {
+        direction = 1;
+    } else if (start < 0){
+        direction = 0;
+    }
+
+    // Thickens the line and draws it
+    pen.lineWidth = 50;
+    pen.stroke();
+
+    // Moves to the next frame
+    requestAnimationFrame(run);
+}
