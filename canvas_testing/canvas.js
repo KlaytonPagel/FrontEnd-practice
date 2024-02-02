51d38fd0d4f6a3251af146d61965ba805c60ede4
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-02 11:52:12 -0600

    Created Canvas Nots
    Created several nots and examples for how to use canvas

--- canvas_testing/canvas.js
+++ canvas_testing/canvas.js
@@ -10,8 +10,31 @@ canvas.height = height;
 // Creates the color of the fill
 pen.fillStyle = "#8f3a3a";
 
+// Creates the color for the lines
+pen.strokeStyle = "#278378";
+
+// starts a drawing pathway
+pen.beginPath();
+
 // Makes a rectangle filled with the fillStyle color
 pen.fillRect(width/2/2, height/2/2, width/2, height/2);
+
+// Makes a line
+pen.moveTo(175, 150) // Sets the start point of the line
+pen.lineTo(300, 150) // Sets the end point of the line
+
+// makes an unfilled rectangle with the x, y, width, height parameters
+pen.rect(50,50,100,100)
+
+// Draws an arc at the x, y, radius, start angle end angle
+pen.moveTo(740, 50)
+pen.arc(700, 50, 40, 0, 2 ) // use 2 * pi end angle for circle
+
+// sets the line width of shape's lines
+pen.lineWidth = 10;
+
+// Draws all shapes, lines, etc
+pen.stroke();
 
 
 
@@ -19,7 +42,7 @@ pen.fillRect(width/2/2, height/2/2, width/2, height/2);
 let start = 0;
 let speed = 3;
 let direction = 0;
-run();
+// run();
 function run(){
     // Clears the last frame and begins a new path
     pen.clearRect(0,0, width, height);
