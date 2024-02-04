dfbddd6cd1dea69625a31017bfef036d682b4536
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-04 16:07:17 -0600

    Basic Collision
    Added very basic collision detection to get a feel for it

--- Simulations (1)/Circle Thing/circle.js
+++ Simulations (1)/Circle Thing/circle.js
@@ -12,10 +12,15 @@ class circleThing {
         this.outerRadius = width / 3;
         this.outerX = width / 2;
         this.outerY = height / 2;
+        this.outerTop = this.outerY - this.outerRadius;
+        this.outerBottom = this.outerY + this.outerRadius;
 
         this.innerRadius = this.outerRadius / 10;
         this.innerX = width / 2;
         this.innerY = height / 2
+        this.innerDirectionY = 1;
+        this.innerDirectionX = 0;
+        this.innerSpeed = 1;
 
         console.log("created circle thing")
     }
@@ -34,9 +39,21 @@ class circleThing {
         this.pen.stroke();
     }
 
+    checkCollision() {
+        if (this.innerY + this.innerRadius > this.outerBottom) {
+            this.innerDirectionY = -1;
+            this.innerSpeed += 0.1
+        } else if (this.innerY - this.innerRadius < this.outerTop) {
+            this.innerDirectionY = 1;
+            this.innerSpeed += 0.1
+        }
+    }
+
     update () {
+        this.checkCollision()
         this.pen.clearRect(0, 0, width, height);
-        this.innerY += 0.5;
+        this.innerY += this.innerSpeed * this.innerDirectionY;
+        this.innerX += 0.5 * this.innerDirectionX;
         this.createInnerCircle();
         this.createOuterCircle();
     }
