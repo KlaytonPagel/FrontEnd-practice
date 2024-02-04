402fad79aa440470b27c1b5b02814d4eb8d33010
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-04 15:44:13 -0600

    Created Animation Loop
    Created the inner circle and and animation loop

--- Simulations/Circle Thing/circle.js
+++ Simulations/Circle Thing/circle.js
@@ -8,16 +8,45 @@ screen.height = height;
 class circleThing {
     constructor(pen) {
         this.pen = pen;
+
+        this.outerRadius = width / 3;
+        this.outerX = width / 2;
+        this.outerY = height / 2;
+
+        this.innerRadius = this.outerRadius / 10;
+        this.innerX = width / 2;
+        this.innerY = height / 2
+
         console.log("created circle thing")
-        this.createOuterCircle()
     }
 
     createOuterCircle() {
-        this.pen.lineWidth = 10;
+        this.pen.lineWidth = 5;
         this.pen. beginPath();
-        this.pen.arc(width/2, height/2, width/3, 0, Math.PI * 2);
+        this.pen.arc(this.outerX, this.outerY, this.outerRadius, 0, Math.PI * 2);
         this.pen.stroke();
+    }
+
+    createInnerCircle() {
+        this.pen.lineWidth = 5;
+        this.pen. beginPath();
+        this.pen.arc(this.innerX, this.innerY, this.innerRadius, 0, Math.PI * 2);
+        this.pen.stroke();
+    }
+
+    update () {
+        this.pen.clearRect(0, 0, width, height);
+        this.innerY += 0.5;
+        this.createInnerCircle();
+        this.createOuterCircle();
     }
 }
 
-new circleThing(pen)
+let circ = new circleThing(pen);
+
+function running() {
+    circ.update();
+    requestAnimationFrame(running);
+}
+
+running();
