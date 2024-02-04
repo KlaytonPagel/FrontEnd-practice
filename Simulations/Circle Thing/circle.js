d10cdeb6547c93875841f593d51d4a41b2bbae2a
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-04 16:25:54 -0600

    Color Changing
    Added a method for changing color of the inner circle upon collision

--- Simulations/Circle Thing/circle.js
+++ Simulations/Circle Thing/circle.js
@@ -8,6 +8,7 @@ screen.height = height;
 class circleThing {
     constructor(pen) {
         this.pen = pen;
+        this.colors = ["red", "blue", "green", "white", "orange", "yellow", "black"]
 
         this.outerRadius = width / 3;
         this.outerX = width / 2;
@@ -20,7 +21,9 @@ class circleThing {
         this.innerY = height / 2
         this.innerDirectionY = 1;
         this.innerDirectionX = 0;
-        this.innerSpeed = 1;
+        this.innerSpeed = 5;
+        this.innerColor = "red";
+        this.innerColorIndex = 0;
 
         console.log("created circle thing")
     }
@@ -33,23 +36,36 @@ class circleThing {
     }
 
     createInnerCircle() {
-        this.pen.lineWidth = 5;
-        this.pen. beginPath();
+        this.pen.lineWidth = 10;
+        this.pen.beginPath();
+        this.pen.fillStyle = this.innerColor
         this.pen.arc(this.innerX, this.innerY, this.innerRadius, 0, Math.PI * 2);
         this.pen.stroke();
+        this.pen.fill();
     }
 
     checkCollision() {
         if (this.innerY + this.innerRadius > this.outerBottom) {
             this.innerDirectionY = -1;
-            this.innerSpeed += 0.1
+            this.innerRadius+= 1
+            this.changeColor()
         } else if (this.innerY - this.innerRadius < this.outerTop) {
             this.innerDirectionY = 1;
-            this.innerSpeed += 0.1
+            this.innerRadius += 1
+            this.changeColor()
         }
     }
 
-    update () {
+    changeColor() {
+        if (this.innerColor === this.colors[this.colors.length - 1]) {
+            this.innerColor = this.colors[0]
+            this.innerColorIndex = 0;
+        } else {
+            this.innerColor = this.colors[this.innerColorIndex += 1]
+        }
+    }
+
+    update() {
         this.checkCollision()
         this.pen.clearRect(0, 0, width, height);
         this.innerY += this.innerSpeed * this.innerDirectionY;
