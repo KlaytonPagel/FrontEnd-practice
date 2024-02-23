10c81bcd8172310f6beca95c902339ed89363442
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-23 15:42:35 -0600

    made object bounce around

--- extensions/TestExtensionOne/script.js
+++ extensions/TestExtensionOne/script.js
@@ -1,4 +1,52 @@
-document.body.style.width = "100px"
-document.body.style.height = "100px"
-console.log(document.body.style.width);
-console.log("dkahsdk");
+screenWidth = 700
+screenHeight = 500
+
+document.body.style.width = screenWidth + "px"
+document.body.style.height = screenHeight + "px"
+
+width = 50
+height = 100
+
+bigA.style.width = width + "px"
+bigA.style.height = height + "px"
+
+bigA = document.getElementById("bigA")
+
+class BigA {
+    constructor() {
+        this.directionX = -1;
+        this.directionY = 1;
+        this.top = 0;
+        this.left = 0;
+    }
+
+    move () {
+        this.top += this.directionY
+        this.left += this.directionX
+
+        bigA.style.top = this.top + "px"
+        bigA.style.left = this.left + "px"
+    }
+
+    checkDirection () {
+        if ((this.top + height > screenHeight) || (this.top < 0)) {
+            this.directionY *= -1
+        }
+        if ((this.left + width > screenWidth) || (this.left < 0)) {
+            this.directionX *= -1
+        }
+    }
+
+    run () {
+        this.checkDirection()
+        this.move()
+    }
+}
+
+big = new BigA()
+
+function running () {
+    big.run()
+    requestAnimationFrame(running)
+}
+running()
