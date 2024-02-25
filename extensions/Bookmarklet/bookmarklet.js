e589e79750d4d48280fb6b362900aa8db8b40613
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-24 20:36:35 -0600

    Bookmarklet Work
    moved files to clean everything up. Did a little work on bookmarklet

--- extensions/Bookmarklet/bookmarklet.js
+++ extensions/Bookmarklet/bookmarklet.js
@@ -1,7 +1,7 @@
 (function (){
     let paragraphs = document.getElementsByClassName('main')
     for (let index = 0; index < paragraphs.length; index++){
-        paragraphs[index].style.backgroundColor = "#13bff5";
+        paragraphs[index].style.backgroundColor = "#424242";
         paragraphs[index].style.width = "100px";
         paragraphs[index].style.height = "100px";
         alert('changed')
