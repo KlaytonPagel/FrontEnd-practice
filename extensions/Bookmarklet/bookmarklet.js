b5300436a34a03d62d7c6d49e80e7e2cf6e5c88a
Klayton Pagel <pagelk@lopers.unk.edu>
2024-02-28 18:55:24 -0600

    Circle Color changing

--- extensions/Bookmarklet/bookmarklet.js
+++ extensions/Bookmarklet/bookmarklet.js
@@ -2,11 +2,14 @@
     // Set variables for all elements you want to change
     const divs = document.getElementsByTagName('div');
     const ps = document.getElementsByTagName('p');
-    const spans = document.getElementsByTagName('spans');
+    const spans = document.getElementsByTagName('span');
     const as = document.getElementsByTagName('a');
+    const iframe = document.getElementsByTagName('iframe');
+    const x_tab_inner = document.getElementsByClassName('x-tab-inner');
+    const x_tab_inner_default = document.getElementsByClassName('x-tab-inner-default');
 
     // Store all elements in an array
-    const elements = [divs, ps, spans, as];
+    const elements = [divs, ps, spans, as, x_tab_inner, x_tab_inner_default, iframe];
 
     // Loops through them changing values
     for (let item = 0; item < elements.length; item++) {
